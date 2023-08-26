483e45b78c8a0da5076c0df9ca51cdd464068e9d
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-08-26 23:04:15 +0200

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,39 @@
-export default function HomePage() {}
+import Link from "next/link";
+import Image from "next/image";
+import hero from "../../public/assets/hero.png";
+
+export default function HomePage() {
+  return (
+    <div className="grid-halves h-screen-navbar">
+      <div className="bg-teal border-right">
+        <div className="column-padding">
+          <div className="tablet-centered">
+            <div className="content-grid home-hero">
+              <h1>
+                You want it? <br /> We got it.
+              </h1>
+              <p className="section-subtitle">
+                All you need in one place, not lying.
+              </p>
+
+              <Link href="/products">
+                <div className="large-button-text">Explore our Products</div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-salmon">
+        <div className="column-padding centered">
+          <div className="callout-wrap">
+            <Image
+              src={hero}
+              className="callout-image"
+              alt="hero-image"
+            ></Image>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
